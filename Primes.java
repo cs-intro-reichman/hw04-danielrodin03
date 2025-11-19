79ae859f976505c3c240eb12c0517a8ee3d38a05
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2025-11-19 13:22:36 +0200

    hw done

--- Primes.java
+++ Primes.java
@@ -1,5 +1,15 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        for (int i = 0; i < Integer.parseInt(args [0]); i++) {
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+    public static boolean isPrime(int i) {
+        if (i <= 1) return false;
+        if (i == 2 || i == 3) return true;
+        for (int j = 2; j*j <= i; j++) {
+            if (i % j == 0) return false;
+        }
+        return true;
     }
 }
